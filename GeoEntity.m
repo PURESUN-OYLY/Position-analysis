25bf8602ed871aae9df15965f8e44febbe93d6c9
PURESUN <mrouyang@live.com>
2026-06-29 19:09:30 +0800

    Add enumeration of entity types

--- GeoEntity.m
+++ GeoEntity.m
@@ -1,42 +1,35 @@
 classdef GeoEntity < handle
     enumeration
-        % r: radius, h:height, t:top, b:bottom
-        % x:axis-x, y:axis-y, z:axis-z
-        cylinder;   % [r]
-        sphere;     
-        cube;
-        cycle;
-        triangle;
-        tetragonum;
+        notdefined({0, 'undefined'})    % [id, name]
+        cylinder({1, 'cylinder'})       % [id, name]
+        sphere({2, 'sphere'})           % [id, name]
+        cube({3, 'cube'})               % [id, name]
+        cycle({4, 'cycle'})             % [id, name]
+        triangle({5, 'triangle'})       % [id, name]
+        tetragonum({6, 'tetragonum'})   % [id, name]
     end
 
     properties
+        % Default type is undefined, which means the entity is not initialized
+        type = {};
+
+        % r: radius, h:height, t:top, b:bottom
+        % x:axis-x, y:axis-y, z:axis-z
         pos(1, 3) double = [0, 0, 0];               % The center of mass of entity
-        cen(1, 6) double = [0, 0, 0, 0, 1, 1, 1];   % Top and bottom face center of entity
+        cen(1, 6) double = [0, 0, 0, 1, 1, 1];      % Top and bottom face center of entity
         rad(1, 2) double = [1, 1];                  % Top and bottom radius of entity
         rot(1, 3) double = [0, 0, 0];               % Posture of entity[x, y, z]
-        color(1, 3) double = [0,4, 0,6, 0.8];       % The color of entity
+        color(1, 3) double = [0.4, 0.6, 0.8];       % The color of entity
     end
 
     methods
+        % Constructor, initialize the entity with default values
         function obj = GeoEntity(type)
-            if ~obj.isValid(type)
-                disp([type 'is not allowed'])
-                disp(['Only support: ', obj.getAll()])
-            end
+            obj.type = type;
         end
     end
 
     methods (Static)
-        function flag = isValid(name)
-            try
-                ShapeType(name);
-                flag = true;
-            catch
-                flag = false;
-            end
-        end
-        
         function list = getAll()
             e = enumeration('ShapeType');
             list = string(e);
--- Main.m
+++ Main.m
@@ -1,1 +1,17 @@
-obj1 = GeoEntity(GeoEntity.box)
+
+%% Test GeoEntity class
+
+% Clear the workspace
+clear; clc; close all;
+
+% obj1 = GeoEntity(GeoEntity.sphere)
+
+% disp(['Create ', GeoEntity.sphere{2}])
+
+% Test the constructor
+obj1 = GeoEntity(GeoEntity.sphere);
+disp(obj1.type{2})
+
+
+
+% disp(GeoEntity.getAll())
